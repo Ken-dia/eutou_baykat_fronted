76580f9f007f793170e65d8cb24fddc193cfa2bb
ken-dia <diahamidou10@gmail.com>
2022-06-30 22:37:16 +0000

    Add parametres to deploy heroku

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.use(express.static('./dist/'));
+app.use(express.static(__dirname + '/dist/eutou-baykat-frontend'));
 
-app.get('/*', (req, res) => {
-  res.sendFile('index.html', { root: 'dist/' });
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/eutou-baykat-frontend/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT);
