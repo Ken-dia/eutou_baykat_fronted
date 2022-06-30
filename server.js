614f1639f73e0544dbe2591ea29795522df94b20
ken-dia <diahamidou10@gmail.com>
2022-06-30 12:26:24 +0000

    deploiement sur heroku edited server file

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/eutou-baykat-frontend'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/eutou-baykat-frontend/index.html') );});
+
+app.use(express.static(__dirname));
+
+//app.use(express.static(__dirname + '/dist/eutou-baykat-frontend'));
+app.get('/', function(req,res) {
+  res.sendFile(path.join(__dirname, './public', 'eutou-baykat-frontend.html'));
+//res.sendFile(path.join(__dirname + '/dist/eutou-baykat-frontend/index.html') );
+});
 app.listen(process.env.PORT || 8080);
