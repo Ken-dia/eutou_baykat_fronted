a87e7199e97f08faa946ce2ca5b608c06a5e45f2
ken-dia <diahamidou10@gmail.com>
2022-06-30 13:45:41 +0000

    Updates to make application go live

--- server.js
+++ server.js
@@ -2,10 +2,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res, next) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
+});
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/eutou-baykat-frontend'));
+/* app.use(express.static(__dirname + '/dist/eutou-baykat-frontend'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/eutou-baykat-frontend/index.html'));
-});
+}); */
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
