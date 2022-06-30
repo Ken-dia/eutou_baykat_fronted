e7eed7189e98a8dad74a2039e8b2baf9fb3ae1b8
ken-dia <diahamidou10@gmail.com>
2022-06-30 16:51:42 +0000

    parametrage de l'application pour le deploiement sur heroku edited

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require("express");
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/eutou-baykat-frontend'));
+app.use(express.static('./dist/eutou-baykat-frontend'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/eutou-baykat-frontend/index.html'));
+  res.sendFile('index.html', { root: 'dist/eutou-baykat-frontend' });
 })
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 8080);
