a95d571b14d94ed9ae5bc5d5f3ea45150c021319
ken-dia <diahamidou10@gmail.com>
2022-08-11 15:14:15 +0000

    menu compte validé

--- src/app/frontoffice/pages/shared-components/navbar/navbar.component.ts
+++ src/app/frontoffice/pages/shared-components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { StorageService } from 'src/app/frontoffice/services/auth/token-storage.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,14 +9,78 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
   menuIcon: boolean = false;
-  constructor(private router: Router) {}
+  constructor(private router: Router, private storageService: StorageService) {}
+  currentUser?: any ;
+  isLoggedIn = false;
+  isAdmin = false;
+  isVendeur = false;
+  menus?: any;
+  menuAdmin = [
+    {
+    icon : 'dashboard',
+    name: 'Dashboard',
+    link: '/dashboard/admin/stats'
+    },
+    {
+      icon : 'group',
+      name: 'Utilisateurs',
+      link: '/dashboard/admin/users'
+    },
+    {
+      icon : 'manage_accounts',
+      name: 'Roles',
+      link: '/dashboard/admin/roles'
+    },
+    {
+      icon : 'label',
+      name: 'Catégories',
+      link: '/dashboard/admin/categories'
+    },
+    {
+      icon : 'bookmarks',
+      name: 'Produits',
+      link: '/dashboard/admin/produits'
+    }
+  ];
+  menuUser = [
+    {
+    icon : 'dashboard',
+    name: 'Dashboard',
+    link: '/dashboard/user/stats'
+    },
+    {
+      icon : 'campaign',
+      name: 'Créer une annonce',
+      link: '/dashboard/user/creer-annonce'
+    },
+    {
+      icon : 'manage_history',
+      name: 'Gérer mes annonces',
+      link: '/dashboard/user/mes-annonces'
+    },
+    {
+      icon : 'favorite',
+      name: 'Mes favoris',
+      link: '/dashboard/user/favoris'
+    },
+  ];
+  ngOnInit(): void {
+    this.isLoggedIn = this.storageService.isLoggedIn();
+    if(this.isLoggedIn) {
+      this.currentUser = this.storageService.getUser();
+      this.isAdmin = this.currentUser.roles.includes("ROLE_ADMIN");
+      this.menus = this.isAdmin ? this.menuAdmin : this.menuUser;
 
-  ngOnInit(): void {}
+    }
+  }
   login() {
     this.router.navigate(['login']);
   }
 
   toggle() {
     this.menuIcon = !this.menuIcon;
+  }
+  goTo(link: string) {
+    this.router.navigate([link]);
   }
 }
